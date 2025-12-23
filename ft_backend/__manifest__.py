12e79c596c38fe277aef5c0ee018355f6ab99702
Ajay Rakholiya <ajayrakholiya7@gmail.com>
2025-12-23 21:32:36 +0530

    Invoice Report

---
+++ ft_backend/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Fama Backend",
+    "version": "19.0.1.0.0",
+    "depends": ["account"],
+    "data": [
+        "views/report_action.xml",
+        "reports/invoice_report.xml"
+    ],
+    "installable": True
+}
