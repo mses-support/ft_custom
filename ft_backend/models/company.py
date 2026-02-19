d6aa468303e833be7d8802da278d6924fc025506
Ajay Rakholiya <ajayrakholiya7@gmail.com>
2026-02-19 18:57:01 +0530

    Update Report

--- ft_backend/models/company.py
+++ ft_backend/models/company.py
@@ -11,53 +11,61 @@ class ResCompany(models.Model):
         related="partner_id.name_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     street_arabic = fields.Char(
         string="Street Arabic",
         related="partner_id.street_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     street2_arabic = fields.Char(
         string="Street2 Arabic",
         related="partner_id.street2_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     city_arabic = fields.Char(
         string="City Arabic",
         related="partner_id.city_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     zip_arabic = fields.Char(
         string="ZIP Arabic",
         related="partner_id.zip_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     country_arabic = fields.Char(
         string="Country Arabic",
         related="partner_id.country_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     address_arabic = fields.Text(
         string="Address Arabic",
         related="partner_id.address_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     company_registry_arabic = fields.Char(
         string="CR No. Arabic",
         related="partner_id.company_registry_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
     vat_arabic = fields.Char(
         string="VAT Arabic",
         related="partner_id.vat_arabic",
         readonly=False,
         store=True,
+        required=True,
     )
-
